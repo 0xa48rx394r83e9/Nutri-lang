3e2ca96f5f054a4fb0a8ee349d4dcb2f6d97411d
Joseph Paulmer <Joseph.paulmer@gmail.com>
2024-05-13 14:04:07 +0800

    Added lexer.h ~ header file

---
+++ lexer.h
@@ -0,0 +1,44 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum
+{
+    TOKEN_VAR,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_WHILE,
+    TOKEN_FOR,
+    TOKEN_FUNC,
+    TOKEN_RETURN,
+    TOKEN_IDENTIFIER,
+    TOKEN_INT,
+    TOKEN_FLOAT,
+    TOKEN_BOOL,
+    TOKEN_STRING,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_MULTIPLY,
+    TOKEN_DIVIDE,
+    TOKEN_EQUAL,
+    TOKEN_NOT_EQUAL,
+    TOKEN_LESS,
+    TOKEN_GREATER,
+    TOKEN_LESS_EQUAL,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_SEMICOLON,
+    TOKEN_EOF
+} TokenType;
+
+typedef struct
+{
+    TokenType type;
+    char *value;
+} Token;
+
+Token *lexer(char *input);
+
+#endif
